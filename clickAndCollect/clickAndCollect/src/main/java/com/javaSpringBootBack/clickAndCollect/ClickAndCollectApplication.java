de8fd3d46a9c0a9e208f5ad7f84d7c6421189a43
lina-22 <haquefahinur@gmail.com>
2023-01-01 20:41:13 +0100

    Bismillahir rahmanir rahim first git commit at 2023

---
+++ clickAndCollect/clickAndCollect/src/main/java/com/javaSpringBootBack/clickAndCollect/ClickAndCollectApplication.java
@@ -0,0 +1,13 @@
+package com.javaSpringBootBack.clickAndCollect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickAndCollectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickAndCollectApplication.class, args);
+	}
+
+}
---
+++ clickAndCollect/clickAndCollect/src/test/java/com/javaSpringBootBack/clickAndCollect/ClickAndCollectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaSpringBootBack.clickAndCollect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickAndCollectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
