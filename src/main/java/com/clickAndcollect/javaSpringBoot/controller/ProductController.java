10b6e75b108aee3c8b429ceff13978eabd09bceb
lina-22 <haquefahinur@gmail.com>
2023-01-28 03:45:08 +0100

    Product controller working

---
+++ src/main/java/com/clickAndcollect/javaSpringBoot/controller/ProductController.java
@@ -0,0 +1,82 @@
+package com.clickAndcollect.javaSpringBoot.controller;
+
+import com.clickAndcollect.javaSpringBoot.dto.ProductDto;
+import com.clickAndcollect.javaSpringBoot.manager.ProductManager;
+import com.clickAndcollect.javaSpringBoot.model.ProductModel;
+import com.clickAndcollect.javaSpringBoot.response.MessageResponse;
+
+import com.clickAndcollect.javaSpringBoot.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/product")
+class ProductController {
+//    @Autowired
+//    ProductService mProductService;
+//
+//    @PostMapping("create")
+//    ResponseEntity<?> create(@RequestBody ProductDto dto){
+//        MessageResponse validation = ProductManager.validation(dto);
+//        if(!validation.isSuccess()){
+//            return ResponseEntity.badRequest().body(validation);
+//        }else {
+//            ProductModel productModel = new ProductModel();
+//            productModel.setName(dto.getName());
+//            ProductModel isSaved =  mProductService.insert(productModel);
+//            return ResponseEntity.ok(isSaved);
+//
+//        }
+//    }
+//
+//    @GetMapping("showOne")
+//    ResponseEntity<?> read(@RequestBody  ProductDto dto){
+//        MessageResponse  validation = ProductManager.readValidation(dto);
+//        if(!validation.isSuccess()){
+//            return ResponseEntity.badRequest().body(validation);
+//        }else {
+//            ProductModel productModel = new ProductModel();
+//            productModel.setName(dto.getId());
+//            productModel.setName(dto.getName());
+//            ProductModel isSaved =  mProductService.update(productModel);
+//            return ResponseEntity.ok(isSaved);
+//        }
+//    }
+//
+//    @GetMapping("showAll")
+//    ResponseEntity<?> showAll(){
+//        return ResponseEntity.ok(mProductService.findAll());
+//    }
+//
+//
+//    @PutMapping("update")
+//    ResponseEntity<?> update(@RequestBody  ProductDto dto){
+//        MessageResponse  validation = ProductManager.updatedValidation(dto);
+//        if(!validation.isSuccess()){
+//            return ResponseEntity.badRequest().body(validation);
+//        }else {
+//            ProductModel productModel = new ProductModel();
+//            productModel.setName(dto.getId());
+//            productModel.setName(dto.getName());
+//            ProductModel isSaved =  mProductService.update(productModel);
+//            return ResponseEntity.ok(isSaved);
+//        }
+//    }
+//
+//    @PostMapping("delete")
+//    ResponseEntity<?> delete(@RequestBody  ProductDto dto){
+//        MessageResponse  validation = ProductManager.deletedValidation(dto);
+//        if(!validation.isSuccess()){
+//            return ResponseEntity.badRequest().body(validation);
+//        }else {
+//            ProductModel productModel = new ProductModel();
+//            productModel.setName(dto.getId());
+//            productModel.setName(dto.getName());
+//            ProductModel isSaved =  mProductService.update(productModel);
+//            return ResponseEntity.ok(isSaved);
+//        }
+//    }
+
+}
