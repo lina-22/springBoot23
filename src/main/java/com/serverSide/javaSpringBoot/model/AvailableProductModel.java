a1bd77e9f2de60a5fd8124e61f5b780396ed2d33
lina-22 <haquefahinur@gmail.com>
2023-03-11 00:54:11 +0100

    AvailableProductModel Create

--- src/main/java/com/serverSide/javaSpringBoot/model/AvailableProductModel.java
+++ src/main/java/com/serverSide/javaSpringBoot/model/AvailableProductModel.java
@@ -10,32 +10,32 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-//@Entity
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Table(name="AvailableProduct")
-//@IdClass(AvailableProductPKId.class) // id object
-//public class AvailableProductModel {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Id
-//    @ManyToOne
-//    @JoinColumn(name = "pa_fk", referencedColumnName = "id")
-//    private ProductModel productModel;
-//
-//    @Column(name = "colour", nullable = true)
-//    private String colour;
-//
-//    @Column(name="quantity", nullable = true)
-//    private String quantity;
-//
-//    @Column(name="size", nullable = true)
-//    private String size;
-//
-//
-//}
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="AvailableProduct")
+@IdClass(AvailableProductPKId.class) // id object
+public class AvailableProductModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "pa_fk", referencedColumnName = "id")
+    private ProductModel productModel;
+
+    @Column(name = "colour", nullable = true)
+    private String colour;
+
+    @Column(name="quantity", nullable = true)
+    private String quantity;
+
+    @Column(name="size", nullable = true)
+    private String size;
+
+
+}
