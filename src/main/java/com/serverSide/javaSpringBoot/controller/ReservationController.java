f6b460cddcd852b6c2bfbe6bd5d831e5a056286b
lina-22 <haquefahinur@gmail.com>
2023-03-12 19:32:23 +0100

    reservation api done

--- src/main/java/com/serverSide/javaSpringBoot/controller/ReservationController.java
+++ src/main/java/com/serverSide/javaSpringBoot/controller/ReservationController.java
@@ -1,8 +1,11 @@
 package com.serverSide.javaSpringBoot.controller;
+import com.serverSide.javaSpringBoot.dto.CategoryDto;
 import com.serverSide.javaSpringBoot.dto.ReservationDto;
 import com.serverSide.javaSpringBoot.manager.ReservationManager;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/reservation")
@@ -13,5 +16,10 @@ public class ReservationController {
     @PostMapping
     public ReservationDto createReservation(@RequestBody ReservationDto reservationDto){
         return reservationManager.createReservation(reservationDto);
+    }
+
+    @GetMapping
+    public List<ReservationDto> getAllReservation(){
+        return reservationManager.getAllReservation();
     }
 }
