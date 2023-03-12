09b0b0eb5c44dbaba940bd72d53fa6fdf5460a82
lina-22 <haquefahinur@gmail.com>
2023-03-12 19:23:57 +0100

    getAllRoles done

--- src/main/java/com/serverSide/javaSpringBoot/manager/RolesManager.java
+++ src/main/java/com/serverSide/javaSpringBoot/manager/RolesManager.java
@@ -1,11 +1,16 @@
 package com.serverSide.javaSpringBoot.manager;
 
+import com.serverSide.javaSpringBoot.dto.ReservationDto;
 import com.serverSide.javaSpringBoot.dto.RolesDto;
 import com.serverSide.javaSpringBoot.dto.UsersDto;
+import com.serverSide.javaSpringBoot.model.ReservationModel;
 import com.serverSide.javaSpringBoot.model.RolesModel;
 import com.serverSide.javaSpringBoot.model.UsersModel;
 import com.serverSide.javaSpringBoot.services.RolesService;
 import com.serverSide.javaSpringBoot.services.UsersService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RolesManager {
 
@@ -15,6 +20,17 @@ public class RolesManager {
         RolesModel addedRoles = rolesService.create(rolesToAdd);
 
         return toRolesDto(addedRoles);
+    }
+
+
+
+    public List<RolesDto> getAllRoles() {
+        List<RolesDto>rolesDtoList = new ArrayList<>();
+        List<RolesModel> rolesModelList = rolesService.findAll();
+        rolesModelList.forEach(data-> {
+            rolesDtoList.add(toRolesDto(data));
+        });
+        return rolesDtoList;
     }
     // ******************* the dto to model data transfer****************
     public RolesModel toRolesModel(RolesDto rolesDto){
