f2e4048829726b5b18e8f1dbc530c4ce517d03c3
lina-22 <haquefahinur@gmail.com>
2023-03-12 04:13:33 +0100

    RolesManager create method done

--- src/main/java/com/serverSide/javaSpringBoot/manager/RolesManager.java
+++ src/main/java/com/serverSide/javaSpringBoot/manager/RolesManager.java
@@ -4,9 +4,18 @@ import com.serverSide.javaSpringBoot.dto.RolesDto;
 import com.serverSide.javaSpringBoot.dto.UsersDto;
 import com.serverSide.javaSpringBoot.model.RolesModel;
 import com.serverSide.javaSpringBoot.model.UsersModel;
+import com.serverSide.javaSpringBoot.services.RolesService;
+import com.serverSide.javaSpringBoot.services.UsersService;
 
 public class RolesManager {
 
+    private RolesService rolesService;
+    public RolesDto createRoles(RolesDto rolesDto){
+        RolesModel rolesToAdd = toRolesModel(rolesDto);
+        RolesModel addedRoles = rolesService.create(rolesToAdd);
+
+        return toRolesDto(addedRoles);
+    }
     // ******************* the dto to model data transfer****************
     public RolesModel toRolesModel(RolesDto rolesDto){
         RolesModel rolesModel = new RolesModel();
@@ -15,7 +24,7 @@ public class RolesManager {
         return rolesModel;
     }
 
-    public RolesDto toUsersDto(RolesModel rolesModel){
+    public RolesDto toRolesDto(RolesModel rolesModel){
         RolesDto rolesDto = new RolesDto();
         rolesDto.setId(rolesModel.getId());
         rolesDto.setName(rolesModel.getName());
