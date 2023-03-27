e3c99f1e44addf5e1dbd017b44cf3ce38bb573dd
lina-22 <haquefahinur@gmail.com>
2023-03-27 15:09:40 +0200

    working on size tables

---
+++ src/main/java/com/serverSide/javaSpringBoot/manager/SizeManager.java
@@ -0,0 +1,17 @@
+package com.serverSide.javaSpringBoot.manager;
+
+
+import com.serverSide.javaSpringBoot.services.RolesService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SizeManager {
+
+    private SizeManager sizeManager;
+
+    // ******************* the dto to model data transfer****************
+
+    public SizeModel to
+}
