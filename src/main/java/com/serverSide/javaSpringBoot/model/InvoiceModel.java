01c5486ea9aedf3a394624bb0c8a33d4e48aac80
lina-22 <haquefahinur@gmail.com>
2023-04-08 22:25:36 +0200

    InvoiceModel init

---
+++ src/main/java/com/serverSide/javaSpringBoot/model/InvoiceModel.java
@@ -0,0 +1,45 @@
+package com.serverSide.javaSpringBoot.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "invoice")
+public class InvoiceModel {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private Date createDate;
+
+    @Column
+    private Date updateDate;
+
+    @Column
+    private String reference;
+
+    @Column
+    private String status;
+
+    @Column
+    private BigDecimal tax;
+
+    @Column
+    private BigDecimal subTotal;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_res_id", referencedColumnName = "reservation_id")
+    private ReservationModel reservationModel;
+
+}
