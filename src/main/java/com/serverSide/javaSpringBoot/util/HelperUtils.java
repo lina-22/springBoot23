20c120a24c45b5178ec2679038be64fadc1f4a5a
lina-22 <haquefahinur@gmail.com>
2023-05-07 09:49:20 +0200

    util/HelperUtils added

--- src/main/java/com/serverSide/javaSpringBoot/util/HelperUtils.java
+++ src/main/java/com/serverSide/javaSpringBoot/util/HelperUtils.java
@@ -5,4 +5,5 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 
 public class HelperUtils {
     public static final ObjectWriter JSON_WRITER = new ObjectMapper().writer().withDefaultPrettyPrinter();
+
 }
